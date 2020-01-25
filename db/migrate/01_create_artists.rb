d7bb1192c2539e3d9c9ed233ad2aa1b798ed1b8b
Warren Harper II <warrenharper2@att.net>
2020-01-25 21:29:58 +0000

    Automatically backed up by Learn

--- db/migrate/01_create_artists.rb
+++ db/migrate/01_create_artists.rb
@@ -7,3 +7,16 @@ class CreateArtists < ActiveRecord::Migration[5.2]
   def down
   end
 end
+ActiveRecord::Base.establish_connection(
+  :adapter => "sqlite3",
+  :database => "db/artists.sqlite"
+)
+sql = <<-SQL
+  CREATE TABLE IF NOT EXISTS artists (
+  id INTEGER PRIMARY KEY,
+  name TEXT,
+  genre TEXT,
+  age INTEGER,
+  hometown TEXT
+  )
+SQL
